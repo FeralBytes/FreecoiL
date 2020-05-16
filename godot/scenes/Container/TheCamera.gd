bd28a28a7c3a2fe0594ba2a65ac365f37821c36d
FeralBytes <feralbytes@gmail.com>
2020-05-15 23:49:16 -0800

    Still overall broken, but FreecoiL Java ported to Godot Plugin. This commit just reverts some git merge issues, but via a manual patch to the files.

--- godot/scenes/Container/TheCamera.gd
+++ godot/scenes/Container/TheCamera.gd
@@ -1,7 +1,6 @@
 extends Camera2D
 
 var panning = false
-var panning_speed = 25
 var last_x = 0
 var last_y = 0
 var screen_width = 540
@@ -14,23 +13,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #    pass
-
-func calc_panning(steps_x, steps_y):
-    var horizontal_dir = null
-    var vertical_dir = null
-    var total_x = (steps_x * screen_width)  # + (screen_width / 2)
-    var total_y = (steps_y * screen_height)  # + (screen_height / 2)
-    # Horizontal Panning in the x coordinate.
-    if total_x < self.position.x:  # Negative
-        horizontal_dir = "left"
-    else:  # Positive
-        horizontal_dir = "right"
-    # Vertical Panning in the y coordinate.
-    if total_y < self.position.y:  # Negative
-        vertical_dir = "up"
-    else:
-        vertical_dir = "down"
-    return [vertical_dir, horizontal_dir, total_x, total_y]
     
 func pan_camera(steps_x, steps_y):
     while panning:
@@ -38,36 +20,48 @@ func pan_camera(steps_x, steps_y):
     #get_tree().call_group("Particle", "emit_on", true)
     Settings.Log("Camera: Panning to: " + str(steps_x) + ", " + str(steps_y), "info")
     panning = true
-    var array = calc_panning(steps_x, steps_y)
-    var pan_vertical_direction = array[0]
-    var pan_horizontal_direction = array[1]
-    var total_to_pan_x = array[2]
-    var total_to_pan_y = array[3]
+    var pan_horizontal_direction = null
+    var pan_vertical_direction = null
+    var speed_step = 100
+    var total_to_pan_x = (steps_x * screen_width)  # + (screen_width / 2)
+    var total_to_pan_y = (steps_y * screen_height)  # + (screen_height / 2)
+    print(total_to_pan_x, "  ", total_to_pan_y)
+    # Horizontal Panning in the x coordinate.
+    if steps_x < self.position.x:  # Negative
+        pan_horizontal_direction = "left"
+    else:  # Positive
+        pan_horizontal_direction = "right"
+    # Vertical Panning in the y coordinate.
+    if steps_y < self.position.y:  # Negative
+        pan_vertical_direction = "up"
+    else:
+        pan_vertical_direction = "down"
+    # Do the panning.
     while panning:
         if pan_horizontal_direction == "right":
-            if self.position.x + panning_speed > total_to_pan_x:
+            if self.position.x + speed_step > total_to_pan_x:
                 self.position.x = total_to_pan_x
             else:
-                self.position.x += panning_speed
+                self.position.x += speed_step
         else:
-            if self.position.x - panning_speed < total_to_pan_x:
+            if self.position.x - speed_step < total_to_pan_x:
                 self.position.x = total_to_pan_x
             else:
-                self.position.x -= panning_speed
+                self.position.x -= speed_step
         if pan_vertical_direction == "down":
-            if self.position.y + panning_speed > total_to_pan_y:
+            if self.position.y + speed_step > total_to_pan_y:
                 self.position.y = total_to_pan_y
             else:
-                self.position.y += panning_speed
+                self.position.y += speed_step
         else:
-            if self.position.y - panning_speed < total_to_pan_y:
+            if self.position.y - speed_step < total_to_pan_y:
                 self.position.y = total_to_pan_y
             else:
-                self.position.y -= panning_speed
-        yield(get_tree().create_timer(0.01), "timeout")
+                self.position.y -= speed_step
+        yield(get_tree().create_timer(0.001), "timeout")
         if self.position.x == total_to_pan_x and self.position.y == total_to_pan_y:
             panning = false
-    #yield(get_tree().create_timer(0.5), "timeout")
+    yield(get_tree().create_timer(0.5), "timeout")
     if not panning:
         self.last_x = steps_x
         self.last_y = steps_y
@@ -76,13 +70,3 @@ func pan_camera(steps_x, steps_y):
 
 func return_to_last_pan():
     self.pan_camera(self.last_x, self.last_y)        
-    
-func instant_pan_camera(steps_x, steps_y):
-    var array = calc_panning(steps_x, steps_y)
-    var total_to_pan_x = array[2]
-    var total_to_pan_y = array[3]
-    self.position.x = total_to_pan_x
-    self.position.y = total_to_pan_y
-    self.last_x = steps_x
-    self.last_y = steps_y
-    get_tree().call_group("MenuButtons", "enabled", true)
