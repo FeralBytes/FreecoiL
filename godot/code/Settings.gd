f978ed6f7157acf1efe427d23ff15d764b515e2a
FeralBytes <feralbytes@gmail.com>
2020-05-21 22:41:57 -0800

    0.3.0-dev11 More improvements to the lobby.

--- godot/code/Settings.gd
+++ godot/code/Settings.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const VERSION = "0.3.0-dev10"
+const VERSION = "0.3.0-dev11"
 const DEBUG_LEVELS = ["not_set", "debug", "info", "warning", "error", "critical"]
 const USER_DIR = "user://"
 const GAME_NAME = "FreecoiL"
