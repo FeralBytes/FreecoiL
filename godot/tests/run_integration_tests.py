a8ef0ba03d204dba97f4c55fe23d3cf7ca108eed
FeralBytes <feralbytes@gmail.com>
2020-06-12 09:56:18 -0800

    A complete image change for testing and exporting godot on Gitlab.

--- godot/tests/run_integration_tests.py
+++ godot/tests/run_integration_tests.py
@@ -12,7 +12,7 @@ if getpass.getuser() == "wolfrage":
     app_path = pathlib.Path("~/Apps/Godot/Godot_v" + GODOT_VERSION + "_x11.64").expanduser()
     proj_path = cwd.joinpath("..")
 else:
-    app_path = cwd.joinpath("godot_editor/Godot_v" + GODOT_VERSION + "_linux_headless.64")
+    app_path = cwd.joinpath("godot")
     proj_path = cwd.joinpath("godot")
 
 returncode = subprocess.call([app_path, "-s", "--path", proj_path, "tests/gut_runner_custom.gd", "type=integration"])
--- godot/tests/run_unit_fast_tests.py
+++ godot/tests/run_unit_fast_tests.py
@@ -8,7 +8,7 @@ import sys
 if getpass.getuser() == "wolfrage":
     app_path = pathlib.Path("~/Apps/Godot/Godot_v3.2.2-beta3_x11.64").expanduser()
 else:
-    app_path = pathlib.Path("~/godot_editor/Godot_v3.2.1-stable_linux_headless.64").expanduser()
+    app_path = pathlib.Path("godot").expanduser()
 cwd = pathlib.Path.cwd()
 proj_path = cwd.joinpath("..")
 returncode = subprocess.call([app_path, "-s", "--path", proj_path, "tests/gut_runner_custom.gd", "type=unit", "speed=fast"])
